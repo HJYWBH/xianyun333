fba1ded30ae4eee2e47883321d3b146ca07383b5
Hou <953952333@qq.com>
2020-10-29 21:17:00 +0800

    头尾公共组件完成

--- components/header.vue
+++ components/header.vue
@@ -1,33 +1,70 @@
 <template>
   <div class="container">
-    <div class="mai">
-      <!-- logo部分 -->
-      <div class="logo">
-        <nuxt-link to="/">
-          <img src="http://157.122.54.189:9093/images/logo.jpg" alt="">
-        </nuxt-link>
-      </div>
-      <!-- 首页顶部导航 -->
-      <div class="nav">
-          <nuxt-link to="/">首页</nuxt-link>
-          <nuxt-link to="/post">旅游攻略</nuxt-link>
-          <nuxt-link to="/hotel">酒店</nuxt-link>
-          <nuxt-link to="/air">国内机票</nuxt-link>
-      </div>
-      <!-- 登录信息 -->
-      <div class="login">
+    <el-row type="flex" justify="space-between" class="main">
+        <!-- logo部分 -->
+        <div class="logo">
+          <nuxt-link to="/">
+            <img src="http://157.122.54.189:9093/images/logo.jpg" alt="" />
+          </nuxt-link>
+        </div>
+        <!-- 首页顶部导航 -->
+        <div class="nav">
+          <nuxt-link class="nvas" to="/">首页</nuxt-link>
+          <nuxt-link class="nvas" to="/post">旅游攻略</nuxt-link>
+          <nuxt-link class="nvas" to="/hotel">酒店</nuxt-link>
+          <nuxt-link class="nvas" to="/air">国内机票</nuxt-link>
+        </div>
+        <!-- 登录信息 -->
+        <div class="login">
           <!-- 登录前 -->
           <nuxt-link to="/user/login">登录/注册</nuxt-link>
           <!-- 登录后 -->
           <div v-if="false"></div>
-      </div>
-    </div>
+        </div>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods:{
+
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.container {
+  height: 60px;
+  line-height: 60px;
+  box-shadow: 0 0 3px #ccc;
+  .main{
+    width: 1000px;
+    margin: 0 auto;
+    .logo{
+      width: 156px;
+      height: 60px;
+      margin-right: 20px;
+      img{
+        width: 100%;
+        padding-top: 8px;
+      }
+    }
+    .nav{
+      display: flex;
+      flex: 1;
+      .nvas{
+        padding: 0 20px;
+        &:hover{
+          color: #409eff;
+          border-bottom: 3px solid #409eff;
+        }
+      }
+      .nuxt-link-exact-active{
+        background: #409eff;
+        color: #fff!important;
+      }
+    }
+  }
+}
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,8 @@
     <!-- 头部组件 -->
     <Header />
     <!-- 底部组件 -->
-    <Footer />
     <Nuxt />
+    <Footer />
   </div>
 </template>
 <script>
@@ -37,39 +37,18 @@ html {
   box-sizing: border-box;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
+*{
+  padding: 0;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
+ul,ol,li,input{
+  list-style: none;
+}
+a{
   text-decoration: none;
-  padding: 10px 30px;
+  color: inherit;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+em,i{
+  font-style: normal;
 }
 </style>
